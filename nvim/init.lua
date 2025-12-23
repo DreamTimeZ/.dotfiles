a8b0b67247993ed3a6c9869e30d5911197f14b2b
Oliver Kiel <olli.k.2001@gmail.com>
2025-12-23 13:16:54 +0100

    refactor(nvim): move treesitter config inline with plugin definition
    Move treesitter configuration from separate setup block into the
    plugin definition to improve organization and reduce code duplication.
    Remove emoji comments and clean up formatting for better readability.

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,46 +8,42 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({{
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-}, -- 🌳 Syntax highlighting
-{"nvim-lualine/lualine.nvim"}, -- 📊 Status line
+    branch = "master",
+    event = { "BufReadPost", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "lua", "vim", "vimdoc", "query", "regex",
+                "bash", "fish",
+                "python", "javascript", "typescript", "java", "c", "cpp", "go", "rust",
+                "json", "yaml", "toml", "xml",
+                "markdown", "markdown_inline", "comment",
+                "git_config", "git_rebase", "gitcommit", "gitignore", "diff",
+                "html", "css", "scss", "dockerfile",
+                "sql", "graphql",
+                "make", "cmake"
+            },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                disable = function(_, buf)
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    return ok and stats and stats.size > 100 * 1024
+                end,
+            },
+        })
+    end,
+},
+{"nvim-lualine/lualine.nvim"},
 {
     "nvim-telescope/telescope.nvim",
     dependencies = {"nvim-lua/plenary.nvim"}
-}, -- 🔭 Fuzzy finder
-{"tpope/vim-commentary"}, -- 📝 Quick commenting
-{"nvim-tree/nvim-tree.lua"}, -- 📂 File explorer
-{"lewis6991/gitsigns.nvim"} -- 🟢 Git signs
+},
+{"tpope/vim-commentary"},
+{"nvim-tree/nvim-tree.lua"},
+{"lewis6991/gitsigns.nvim"}
 })
-
--- 🌈 Enable Treesitter Syntax Highlighting
-require("nvim-treesitter.configs").setup {
-    -- Install only parsers you actually use
-    ensure_installed = {
-        "lua", "vim", "vimdoc", "query", "regex",  -- Nvim essentials
-        "bash", "fish",  -- Shells (zsh not available in treesitter)
-        "python", "javascript", "typescript", "java", "c", "cpp", "go", "rust",  -- Common languages
-        "json", "yaml", "toml", "xml", "ini",  -- Config formats
-        "markdown", "markdown_inline", "comment",  -- Documentation
-        "git_config", "git_rebase", "gitcommit", "gitignore", "diff",  -- Git
-        "html", "css", "scss", "dockerfile",  -- Web/DevOps
-        "sql", "graphql",  -- Query languages
-        "make", "cmake"  -- Build tools
-    },
-    -- Auto-install parsers when opening new file types
-    auto_install = true,
-    highlight = {
-        enable = true,
-        -- Disable for very large files (performance)
-        disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end,
-    }
-}
 
 -- 🖱 Enable Mouse Support
 vim.opt.mouse = "a"
